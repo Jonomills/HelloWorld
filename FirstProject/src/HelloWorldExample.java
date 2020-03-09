699792de9b5384c4c5237a8bcd1b404f1ca82b66
Jonomills <jonomills@live.com>
2020-03-09 14:53:47 +1300

    yee

--- FirstProject/src/HelloWorldExample.java
+++ FirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello WOrld to the user.
+		// Output Hello World to the user.
 		System.out.println("Hello World!");
 
 	}
